917764f73e237c243833dfd4f4148c7834ceee54
Tomo Masakura <tomo.masakura@gmail.com>
2015-11-05 16:14:24 +0900

    Add deploy task to gh-pages

---
+++ gulp/deploy.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var runSequence = require('run-sequence');
+
+gulp.task('gh-pages', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('deploy', function () {
+  runSequence('clean', ['html', 'fonts', 'other'] , 'gh-pages');
+});
